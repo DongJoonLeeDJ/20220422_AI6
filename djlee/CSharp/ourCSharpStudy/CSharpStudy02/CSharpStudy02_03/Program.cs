4835a6af72713a2ed355e5fc1c49a6fd5589b72b
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-06-03 14:01:57 +0900

    4번까지 완료

--- djlee/CSharp/ourCSharpStudy/CSharpStudy02/CSharpStudy02_03/Program.cs
+++ djlee/CSharp/ourCSharpStudy/CSharpStudy02/CSharpStudy02_03/Program.cs
@@ -107,6 +107,32 @@ namespace CSharpStudy02_03
                 Console.WriteLine("잘못된 month입니다." + month);
 
             month = DateTime.Now.Month;
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울입니다.");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄입니다.");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름입니다.");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을입니다.");
+                    break;
+
+                default:
+                    break;
+            }
 
 
 
