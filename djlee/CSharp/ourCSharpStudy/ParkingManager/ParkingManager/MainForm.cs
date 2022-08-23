fe6c51cc4a02daaf32c7d56d971bb606c7e0655b
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-08-23 10:14:44 +0900

    Update MainForm.cs

--- djlee/CSharp/ourCSharpStudy/ParkingManager/ParkingManager/MainForm.cs
+++ djlee/CSharp/ourCSharpStudy/ParkingManager/ParkingManager/MainForm.cs
@@ -196,7 +196,8 @@ namespace ParkingManager
                 textBox_driverName.Text = car.driverName;
                 textBox_phoneNumber.Text = car.phoneNumber;
 
-                textBox_findNum.Text = car.ParkingSpot.ToString();
+                textBox_findNum.Text = textBox_parkingSpot.Text;
+                //textBox_findNum.Text = car.ParkingSpot.ToString();
 
             }
             catch (Exception)
