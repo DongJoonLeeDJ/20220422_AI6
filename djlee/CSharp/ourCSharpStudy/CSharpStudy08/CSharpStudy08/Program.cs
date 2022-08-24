17dfe8aeaf7ad9ee122cc6b428094e4f5c1cb040
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-08-24 11:39:23 +0900

    Update Program.cs

--- djlee/CSharp/ourCSharpStudy/CSharpStudy08/CSharpStudy08/Program.cs
+++ djlee/CSharp/ourCSharpStudy/CSharpStudy08/CSharpStudy08/Program.cs
@@ -92,7 +92,46 @@ namespace CSharpStudy08
                 Console.WriteLine(mymoney);
                 item.Sleep();
             }
-            
+
+
+            //다형성을 활용하면 1개의 foreach문,
+            //1개의 List에 이 9개의 데이터를 모두 넣을 수 있다.
+
+            //다형성 : 다양한 형태를 띌 가능성
+            //왼쪽은 조상클래스 오른쪽은 왼쪽의 자손이어 함
+
+            Human ex1 = new Student();
+            Human ex2 = new Worker();
+
+            //ex1, ex2는 모두 Human타입의 변수
+            //그러나 오른쪽 부분이 자손 클래스
+            //즉 인스턴스는 자손클래스로 만들어짐
+            //그래서 자손으로 형변환이 가능함
+            //ex1.Study(5);
+            //오른쪽 부분이 자손이기 때문에 가능
+            ((Student)ex1).Study(5);
+            (ex1 as Student).Sleep();
+
+            //영어로 치면 is 법칙이 성립
+            //Student is Human
+            //오른쪽꺼 is 왼쪽꺼
+            //Worker is Human
+
+            List<Human> people = new List<Human>()
+            { h,h2,h3,s,s2,s3,w,w2,w3 };
+
+            foreach(var item in people)
+            {
+                item.Sleep();
+                if (item is Student)
+                    (item as Student).Study(1);
+                if (item is Worker)
+                {
+                    int m = (item as Worker).Work();
+                    Console.WriteLine(m);
+                }
+            }
+
         }
     }
 }
