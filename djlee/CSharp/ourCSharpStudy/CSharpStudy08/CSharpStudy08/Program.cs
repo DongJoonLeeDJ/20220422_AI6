765639446c0f54ab533447f2c76d9231e6a51b07
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-08-24 10:58:19 +0900

    문제 완료

--- djlee/CSharp/ourCSharpStudy/CSharpStudy08/CSharpStudy08/Program.cs
+++ djlee/CSharp/ourCSharpStudy/CSharpStudy08/CSharpStudy08/Program.cs
@@ -23,8 +23,16 @@ namespace CSharpStudy08
             s.score = 100;
             s.Study(10);
 
-            
-
+            Worker w = new Worker();
+            w.Name = "이상수";
+            w.Age = 30;
+            w.Sleep();
+            //pay 값이 정해진 전후의 Work 출력 비교
+            int money = w.Work(); //이때는 w.pay가 0
+            Console.WriteLine("money="+money);
+            w.pay = 5000;
+            money = w.Work(); //pay가 정해진 시점의 돈
+            Console.WriteLine("money="+money);
         }
     }
 }
---
+++ djlee/CSharp/ourCSharpStudy/CSharpStudy08/CSharpStudy08/Worker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy08
+{
+    public class Worker : Human
+    {
+        //Name, Age, Sleep이 이미 포함되어 있다.
+        public int pay { get; set; }
+        public int Work()
+        {
+            if(pay==0)
+                Console.WriteLine("열정페이 ㅠㅠ");
+            else
+                Console.WriteLine(pay+"만큼 일을 합니다.");
+
+            return pay * 30;
+        }
+    }
+}
