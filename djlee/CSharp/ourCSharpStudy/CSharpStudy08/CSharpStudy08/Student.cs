203f07fbabf51288b3d19dc550e85a2dc093dd4c
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-08-24 10:47:50 +0900

    Create Student.cs

---
+++ djlee/CSharp/ourCSharpStudy/CSharpStudy08/CSharpStudy08/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy08
+{
+    //Student는 Human을 상속받아서
+    //Name과 Age 값도 가지고 있다.
+    //그리고 Sleep도 할 수 있다.
+
+    //:(콜론) = extends(자바) 
+    // (참고)
+    //:(콜론) = implements(자바)
+    public class Student : Human
+    {
+        public int score;
+
+        public void Study(int hour)
+        {
+            Console.WriteLine(score+"점을 받았습니다.");
+            Console.WriteLine(hour+"만큼 공부합니다.");
+        }
+    }
+}
